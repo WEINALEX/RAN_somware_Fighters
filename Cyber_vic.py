5c4f99a12e0ae83c95c342655ee884862166241a
VictorBartolomeo <victor.monteragioni@icloud.com>
2024-12-11 23:20:22 +0100

    Premier commit
    Base cyber

--- Cyber_vic.py
+++ Cyber_vic.py
@@ -1,130 +1,119 @@
 import re
 
- #mot de passe de la salle 202
-mdp_salle101="101"
-#le code se déclenche si le mot de passe de la salle est correct
+#  #mot de passe de la salle 202
+# mdp_salle101="101"
+# mot_de_passe_salle_cyber=input("Entrez le mot de passe de la salle : ")
+# #le code se déclenche si le mot de passe de la salle est correct
 
-if mot_de_passe_salle_cyber==mdp_salle101:
-    print("Bienvenue dans la salle 101")
+# if mot_de_passe_salle_cyber==mdp_salle101:
+#     print("Bienvenue dans la salle 101")
 
 
 #pygame image d'accueil de la salle 101 : Taille 1440x1024px
 #Créer box usb et box mot de passe (sur ordinateur)
+class Cybersecurite:
+  def __init__(self):
+    self.enigme_cyber_1=False
+    self.enigme_cyber_2=False
+    self.enigme_cyber_3=False
+    self.enigme_cyber_4=False
+    self.password=None
 
-def check_password(password):
-  """
-  Vérifie si un mot de passe est sécurisé.
+  def check_password(self, password):
+    if len(password) < 10:
+      return False
+    if not re.search("[a-z]", password):
+      return False
+    if not re.search("[A-Z]", password):
+      return False
+    if not re.search("[0-9]", password):
+      return False
+    if not re.search("[!@#$%^&*()_+=\[\]{};':\"\\|,.<>\/?-]", password):
+      return False
+    return True
 
-  Un mot de passe sécurisé doit comporter au moins 10 caractères, une lettre majuscule, 
-  une lettre minuscule, au moins un chiffre et un caractère spécial.
+  #pygame clic sur le bloc mot de passe
 
-  Args:
-    password: Le mot de passe à vérifier.
+  #si clic sur l'ordinateur, pour changement de mdp : 
+  def change_password_cyber(self):
+    nb_text_mdp_cyber=0
+    while True:
+      password = input("Entrez un nouveau mot de passe : ")
+      if self.check_password(password):
+        print("Mot de passe accepté.")
+        enigme_cyber_2=True
+        break
+      else:
+        print("Mot de passe non sécurisé. Veuillez réessayer.")
+        nb_text_mdp_cyber+=1
+        if nb_text_mdp_cyber>=3:
+          print("Pour rappel, un mot de passe sécurisé doit comporter au moins 10 caractères, une lettre majuscule, une lettre minuscule, au moins un chiffre et un caractère spécial.")
 
-  Returns:
-    True si le mot de passe est sécurisé, False sinon.
-  """
-  if len(password) < 10:
-    return False
-  if not re.search("[a-z]", password):
-    return False
-  if not re.search("[A-Z]", password):
-    return False
-  if not re.search("[0-9]", password):
-    return False
-  if not re.search("[!@#$%^&*()_+=\[\]{};':\"\\|,.<>\/?-]", password):
-    return False
-  return True
+  #pygame clic sur le bloc Phishing
 
+  # def phishing_cyber(self):
+  #   #images des mails
+  #   print("Vous avez reçu un mail suspect.")
 
-enigme_cyber_1=False #USB
-enigme_cyber_2=False #Mot de passe
-enigme_cyber_3=False #Social Engineering
-enigme_cyber_4=False #Phishing
-
-#pygame clic sur le bloc mot de passe
-
-#si clic sur l'ordinateur, pour changement de mdp : 
-nb_text_mdp_cyber=0
-while True:
-  password = input("Entrez un nouveau mot de passe : ")
-  if check_password(password):
-    print("Mot de passe accepté.")
-    enigme_cyber_2=True
-    break
-  else:
-    print("Mot de passe non sécurisé. Veuillez réessayer.")
-    nb_text_mdp_cyber+=1
-    if nb_text_mdp_cyber>=3:
-      print("Pour rappel, un mot de passe sécurisé doit comporter au moins 10 caractères, une lettre majuscule, une lettre minuscule, au moins un chiffre et un caractère spécial.")
-
-#pygame clic sur le bloc Phishing
-
-def phishing_cyber():
-   #images des mails
-  print("Vous avez reçu un mail suspect.")
-
-  enigme_cyber_3=True
+  #   enigme_cyber_3=True
 
 
-#pygame clic sur le bloc USB
+  # #pygame clic sur le bloc USB
 
-def usb_cyber():
-    print("Vous avez trouvé une clé USB.")
-  #choix multiple : si branchement de clef USB afficher un message d'erreur et des rappels sur la cybersécurité, si l'on choisit de laisser la clef USB sans la brancher, afficher un message de félicitations et des informations sur la cybersécurité
-    choix_usb=input("Voulez-vous brancher la clé USB ? (oui/non)")
-    if choix_usb=="oui":
-        print("Erreur : ne branchez jamais une clé USB inconnue sur votre ordinateur.\nLes clés USB peuvent contenir des virus et des logiciels malveillants.")
-    elif choix_usb=="non":
-        print("Félicitations ! Vous avez fait preuve de prudence.\nLes clés USB peuvent contenir des virus et des logiciels malveillants.")
-    enigme_cyber_1=True
-    return
+  # def usb_cyber(self):
+  #     print("Vous avez trouvé une clé USB.")
+  #   #choix multiple : si branchement de clef USB afficher un message d'erreur et des rappels sur la cybersécurité, si l'on choisit de laisser la clef USB sans la brancher, afficher un message de félicitations et des informations sur la cybersécurité
+  #     choix_usb=input("Voulez-vous brancher la clé USB ? (oui/non)")
+  #     if choix_usb=="oui":
+  #         print("Erreur : ne branchez jamais une clé USB inconnue sur votre ordinateur.\nLes clés USB peuvent contenir des virus et des logiciels malveillants.")
+  #     elif choix_usb=="non":
+  #         print("Félicitations ! Vous avez fait preuve de prudence.\nLes clés USB peuvent contenir des virus et des logiciels malveillants.")
+  #     enigme_cyber_1=True
+  #     return
 
-usb_cyber()
+  # if self.enigme_cyber_1==True and self.enigme_cyber_2==True and self.enigme_cyber_3==True:
 
-if enigme_cyber_1==True and enigme_cyber_2==True and enigme_cyber_3==True:
+  # #Apparition du splash screen avec l'individu louche
+  # #pygame clic sur le bloc Social Engineering
+  #   def social_engineering_cyber(self):
+  #     #Présentation type sondage ?
+  #     error_social_engineering=0
+  #     print("Élève MNS : Salut, je suis nouveau! Toi aussi tu es en cybersecurité?")
+  #     print("user.name : Oui, je suis en cybersecurité.")
+  #     print("Élève MNS : C'est pas super facile comme cours, tu ne trouves pas?")
+  #     print("user.name : Si, c'est vrai que c'est un peu difficile.")
+  #     print("Élève MNS : Je suis en réorientation, tu as fais quel lycée toi?")
+  #     if input("Entrez une réponse")==reponse_nathalie_lycee:
+  #         print("Élève MNS : Ah, moi aussi! On a dû se croiser là-bas.")
+  #         error_social_engineering+=1
+  #     else:
+  #       print("Élève MNS : Ah, d'accord ! Je n'étais pas là-bas moi")
+  #     print("Élève MNS : Tu habites dans le coin par hasard? J'ai l'impression de t'avoir déjà croisé")
+  #     if input("Entrez une réponse")==reponse_nathalie_adresse:
+  #         print("Élève MNS : Ah, c'est bien ce que je pensais! On habite dans le même quartier.")
+  #         error_social_engineering+=1
+  #     else: 
+  #         print("Élève MNS : Ah, d'accord ! Je ne suis pas du coin.")
+  #     print("Élève MNS : Tu as des frères et soeurs? Tu as l'air d'avoir une grande famille.")
+  #     if input("Entrez une réponse")==reponse_nathalie_famille:
+  #         print("Élève MNS : Ah, c'est bien ce que je pensais! Tu as une grande famille.")
+  #         error_social_engineering+=1
+  #     else:
+  #         print("Élève MNS : Ah, d'accord ! Pas de soucis.")
+  #     print("Élève MNS : Je n'arrive pas à accéder à mon PC, tu peux me donner ton ID s'il te plait pour que je puisse me connecter ?.")
+  #     if input("Entrez une réponse")==reponse_nathalie_ID:
+  #         print("Élève MNS : Merci beaucoup! Je vais pouvoir me connecter maintenant.")
+  #         error_social_engineering+=1
+  #     else:
+  #       print("Élève MNS : D'accord, je vais demander à quelqu'un d'autre.")
+  #     print("Élève MNS : Bon hé bien je te laisse")
+  #     print("user.name : D'accord, à plus tard!")
 
-#Apparition du splash screen avec l'individu louche
-#pygame clic sur le bloc Social Engineering
-  def social_engineering_cyber():
-    #Présentation type sondage ?
-    error_social_engineering=0
-    print("Élève MNS : Salut, je suis nouveau! Toi aussi tu es en cybersecurité?")
-    print("user.name : Oui, je suis en cybersecurité.")
-    print("Élève MNS : C'est pas super facile comme cours, tu ne trouves pas?")
-    print("user.name : Si, c'est vrai que c'est un peu difficile.")
-    print("Élève MNS : Je suis en réorientation, tu as fais quel lycée toi?")
-    if input("Entrez une réponse")==reponse_nathalie_lycee:
-        print("Élève MNS : Ah, moi aussi! On a dû se croiser là-bas.")
-        error_social_engineering+=1
-    else:
-      print("Élève MNS : Ah, d'accord ! Je n'étais pas là-bas moi")
-    print("Élève MNS : Tu habites dans le coin par hasard? J'ai l'impression de t'avoir déjà croisé")
-    if input("Entrez une réponse")==reponse_nathalie_adresse:
-        print("Élève MNS : Ah, c'est bien ce que je pensais! On habite dans le même quartier.")
-        error_social_engineering+=1
-    else: 
-        print("Élève MNS : Ah, d'accord ! Je ne suis pas du coin.")
-    print("Élève MNS : Tu as des frères et soeurs? Tu as l'air d'avoir une grande famille.")
-    if input("Entrez une réponse")==reponse_nathalie_famille:
-        print("Élève MNS : Ah, c'est bien ce que je pensais! Tu as une grande famille.")
-        error_social_engineering+=1
-    else:
-        print("Élève MNS : Ah, d'accord ! Pas de soucis.")
-    print("Élève MNS : Je n'arrive pas à accéder à mon PC, tu peux me donner ton ID s'il te plait pour que je puisse me connecter ?.")
-    if input("Entrez une réponse")==reponse_nathalie_ID:
-        print("Élève MNS : Merci beaucoup! Je vais pouvoir me connecter maintenant.")
-        error_social_engineering+=1
-    else:
-      print("Élève MNS : D'accord, je vais demander à quelqu'un d'autre.")
-    print("Élève MNS : Bon hé bien je te laisse")
-    print("user.name : D'accord, à plus tard!")
-
-    if error_social_engineering>=:
-      print("Vous avez donné trop d'informations personnelles. Attention au Social Engineering!")
-    else:
-      print("Vous avez fait preuve de prudence. Attention au Social Engineering!")
-    enigme_cyber_4=True
+  #     if error_social_engineering>=2:
+  #       print("Vous avez donné trop d'informations personnelles. Attention au Social Engineering!")
+  #     else:
+  #       print("Vous avez fait preuve de prudence. Attention au Social Engineering!")
+  #     enigme_cyber_4=True
 
    
 
---
+++ main_vic.py
@@ -0,0 +1,61 @@
+import pygame, sys
+from start_vic import EscapeGame
+from Cyber_vic import Cybersecurite
+
+pygame.init()
+
+def main():
+
+        # Main game loop
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    # Handle click on sound button
+                    if play.sound_button_rect.collidepoint(event.pos):
+                        play.music_muted = not play.music_muted
+                        if play.music_muted:
+                            pygame.mixer.music.pause()
+                        else:
+                            pygame.mixer.music.unpause()
+
+                    # Handle click on validation button
+                    if play.validate_button_rect.collidepoint(event.pos):
+                        print(f"Input validated: {play.input_text}")
+                        play.quack_sound.play()  # Play the quack sound
+                        play.input_text = ""  # Clear input text
+                    if play.image_rect.collidepoint(event.pos):
+                        cyber.change_password_cyber()
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        print(f"Input validated: {play.input_text}")
+                        play.quack_sound.play()  # Play the quack sound
+                        play.input_text = ""  # Clear input text
+                    elif event.key == pygame.K_BACKSPACE:
+                        play.input_text = play.input_text[:-1]
+                    else:
+                        play.input_text += event.unicode
+
+            # Draw the background image
+            play.draw_background(play.background_image)
+
+            # Draw the input field
+            play.draw_input_field()
+
+            # Draw the sound button
+            play.draw_sound_button()
+
+            # Update the display
+            pygame.display.flip()
+
+        # Quit Pygame
+        pygame.quit()
+        sys.exit()
+
+if __name__ == "__main__":
+    play = EscapeGame()
+    cyber = Cybersecurite()
+    main()
